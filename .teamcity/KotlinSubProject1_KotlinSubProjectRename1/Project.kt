89d59412272def7cf780f77fdee06027881b3b7a
kseniailina <50660900+kseniailina@users.noreply.github.com>
2020-08-24 12:38:22 +0300

    Update Project.kt

--- .teamcity/KotlinSubProject1_KotlinSubProjectRename1/Project.kt
+++ .teamcity/KotlinSubProject1_KotlinSubProjectRename1/Project.kt
@@ -5,13 +5,11 @@ import KotlinSubProject1_KotlinSubProjectRename1.vcsRoots.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 
-const val REPOSITORY_NAME = "gmat2-thirdparty-swx-qt-build"
-
 object Project : Project({
     uuid = "693f05f0-1bc4-4a65-b417-5c7ca3d77663"
     id("KotlinSubProject1_KotlinSubProjectRename1")
     parentId("KotlinSubProject1")
-    name = REPOSITORY_NAME
+    name = "Kotlin SubProject Rename1"
 
     vcsRoot(KotlinSubProject1_KotlinSubProjectRename1_HttpsGithubComKseniailinaKotlinSubProjectRename1gitRefsHeadsMaster)
 
